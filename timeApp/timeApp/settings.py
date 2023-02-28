b156f2a66cc855ea174f26f0b3623269f8a7e694
Lemson Mureya <lmureya@conncoll.edu>
2023-02-28 16:12:45 -0500

    added gitignore file for env

--- timeApp/timeApp/settings.py
+++ timeApp/timeApp/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '2#dc2zmryhc=3%c^gnel0bmzla2v-%8c0nu9p=r(kmc!c9i1!n'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
